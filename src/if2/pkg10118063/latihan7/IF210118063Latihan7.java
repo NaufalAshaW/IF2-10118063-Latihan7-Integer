7ff11c8b1599397dab57ba0782ac924577dd3e39
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:55:41 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan7/IF210118063Latihan7.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan7;
+
+/**
+ *
+ * @author
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 30224;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("Byte : " +b);
+        System.out.println("Short: " +s);
+        System.out.println("Int  : " +i);
+        System.out.println("Long : " +l);
+    }
+    
+}
